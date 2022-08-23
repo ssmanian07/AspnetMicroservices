09b18a084ad3d7ac61b2ec6f45b9b7e424971e32
ssmanian07 <ssmanianmca@gmail.com>
2022-08-23 20:46:24 +0530

    Catalog API- Added Model, Dal and Business Layer

---
+++ src/services/catalog/Catalog.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,74 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System.Xml.Linq;
+
+namespace Catalog.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext;
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _catalogContext.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _catalogContext.Products.ReplaceOneAsync(
+                filter: p => p.Id == product.Id,
+                replacement: product );
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _catalogContext.Products.DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _catalogContext.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string category)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, category);
+
+            return await GetProductByFilter(filter);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+            return await GetProductByFilter(filter);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Exists(p => true);
+
+            return await GetProductByFilter(filter);
+
+            //return await _catalogContext.Products.Find(P => true).ToListAsync();
+        }
+
+        
+        private async Task<IEnumerable<Product>> GetProductByFilter(FilterDefinition<Product> filter)
+        {
+            return await _catalogContext.Products.Find(filter).ToListAsync();
+        }
+    }
+}
