82e75a1fdfdb030814f3ff6ba288b6a0a3f7724d
ssmanian07 <ssmanianmca@gmail.com>
2022-09-01 15:16:41 +0530

    Discount API

--- src/services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ src/services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -1,19 +1,18 @@
 ﻿using Dapper;
 using Discount.API.Entities;
-using Discount.API.Repositories;
 using Microsoft.AspNetCore.Connections.Features;
 using Npgsql;
 
-namespace Discount.API
+namespace Discount.API.Repositories
 {
     public class DiscountRepository : IDiscountRepository
     {
         private readonly IConfiguration _configuration;
 
-        
+
         public DiscountRepository(IConfiguration configuration)
         {
-            _configuration = configuration;            
+            _configuration = configuration;
         }
 
         public async Task<Coupon> GetCoupon(string productName)
@@ -38,9 +37,12 @@ namespace Discount.API
 
             var affected = await connection.ExecuteAsync
                 ("INSERT INTO Coupon (ProductName,Description,Amount) VALUES (@ProductName,@Description,@Amount)",
-                new { ProductName = coupon.ProductName,
-                Description = coupon.Description,
-                Amount=coupon.Amount});
+                new
+                {
+                    coupon.ProductName,
+                    coupon.Description,
+                    coupon.Amount
+                });
 
             return affected > 0;
         }
@@ -53,10 +55,10 @@ namespace Discount.API
                 ("UPDATE Coupon SET ProductName = @ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id",
                 new
                 {
-                    ProductName = coupon.ProductName,
-                    Description = coupon.Description,
-                    Amount = coupon.Amount,
-                    Id = coupon.Id
+                    coupon.ProductName,
+                    coupon.Description,
+                    coupon.Amount,
+                    coupon.Id
                 });
 
             return affected > 0;
